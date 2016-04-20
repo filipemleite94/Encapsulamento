0a30681f222096ed23c36d519f56b895bdcdcb58
filipemleite94 <filipemleite94@gmail.com>
2016-04-20 07:30:36 -0300

    Atualização 1
    Usou-se o encapsulamento

--- src/time.java
+++ src/time.java
@@ -1,14 +1,24 @@
 
 class time {
-	int hour;
-	int minute;
-	
+	protected int hour;
+	protected int minute;
 	time(){};
-
+	private void hour(int hour){
+		this.hour=hour;
+	}
+	private void minute(int minute){
+		this.minute=minute;
+	}
+	private int hour(){
+		return hour;
+	}
+	private int minute(){
+		return minute;
+	}
 	public static void main(String[] args) {
 		time t=new time();
-		t.hour=3;
-		t.minute=25;
-		System.out.print("The time now is "+t.hour+":"+t.minute);
+		t.hour(3);
+		t.minute(25);
+		System.out.print("The time now is "+t.hour()+":"+t.minute());
 	}
 }
