4bff4c94237dd29b820705b49e1b3ed2d73683dc
filipemleite94 <filipemleite94@gmail.com>
2016-04-20 07:41:10 -0300

    Atualização 2
    Adicionado comentários a classe time

--- src/time.java
+++ src/time.java
@@ -1,20 +1,31 @@
 
 class time {
+	//inicializar variaveis
 	protected int hour;
 	protected int minute;
 	time(){};
+	
+	//recebe a hora
 	private void hour(int hour){
 		this.hour=hour;
 	}
+	
+	//recebe os minutos
 	private void minute(int minute){
 		this.minute=minute;
 	}
+	
+	//retorna a hora
 	private int hour(){
 		return hour;
 	}
+	
+	//retorna os minutos
 	private int minute(){
 		return minute;
 	}
+	
+	//main
 	public static void main(String[] args) {
 		time t=new time();
 		t.hour(3);
